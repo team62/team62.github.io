a684e5464edda5569304b463af524815248019b3
Jonathan Damico <jj_damico@yahoo.com>
2016-02-07 20:15:37 -0800

    try this new dank fresh way of retrieving data

--- js.js
+++ js.js
@@ -203,7 +203,7 @@ $(document).ready(function () {
     $.ajax({
             url: 'http://api.vexdb.io/get_skills?type=0&sku=' + mySKU,
             dataType: 'json',
-            success: function (jd) {
+            success: function (jd) {amico
                 roboSkillsHtml = '<table style="width:100%" border="1"><tr><th>Rank</th><th>Team #</th><th>Score</th><th>Attempts</th></tr>';
                 for (i = 0; i < jd.size; i++) {
                   if(jd.result[i].team == teamNumber) {
@@ -223,6 +223,7 @@ $(document).ready(function () {
             },
             async: false,
         });
+        /* old skills
         $.ajax({
                 url: 'http://api.vexdb.io/get_skills?type=1&sku=' + mySKU,
                 dataType: 'json',
@@ -246,6 +247,31 @@ $(document).ready(function () {
                 },
                 async: false,
             });
+            */
+            $.ajax({
+                    url: 'http://ajax.robotevents.com/tm/results/skills_programming/?format=csv&sku=' + mySKU+'&div=',
+                    dataType: 'text',
+                    success: function (input) {
+                        jd = CSV2JSON(input);
+                        roboSkillsHtml = '<table style="width:100%" border="1"><tr><th>Rank</th><th>Team #</th><th>Score</th><th>Attempts</th></tr>';
+                        for (i = 0; i < jd.size; i++) {
+                          if(jd[i].team == teamNumber) {
+                            roboSkillsHtml+=('<td><b>' + jd.result[i].rank + '</b></td>');
+                            roboSkillsHtml+=('<td><b>' + jd.result[i].team + '</b></td>');
+                            roboSkillsHtml+=('<td><b>' + jd.result[i].highscore + '</b></td>');
+                            roboSkillsHtml+=('<td><b>' + jd.result[i].attempts + '</b></td></tr>');
+                          } else {
+                            roboSkillsHtml+=('<td>' + jd.result[i].rank + '</td>');
+                            roboSkillsHtml+=('<td>' + jd.result[i].team + '</td>');
+                            roboSkillsHtml+=('<td>' + jd.result[i].highscore + '</td>');
+                            roboSkillsHtml+=('<td>' + jd.result[i].attempts + '</td></tr>');
+                          }
+                        }
+                        roboSkillsHtml+= '</table>';
+                        $('#progskills').append(roboSkillsHtml);
+                    },
+                    async: false,
+                });
 $.ajax({
         url: ('http://api.vexdb.io/get_matches?sku=' + mySKU),
         dataType: 'json',
@@ -262,3 +288,72 @@ $.ajax({
        // async: false,
     });
 });
+
+function CSVToArray(strData, strDelimiter) {
+    // Check to see if the delimiter is defined. If not,
+    // then default to comma.
+    strDelimiter = (strDelimiter || ",");
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp((
+    // Delimiters.
+    "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+    // Quoted fields.
+    "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+    // Standard fields.
+    "([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    var arrData = [[]];
+    // Create an array to hold our individual pattern
+    // matching groups.
+    var arrMatches = null;
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while (arrMatches = objPattern.exec(strData)) {
+        // Get the delimiter that was found.
+        var strMatchedDelimiter = arrMatches[1];
+        // Check to see if the given delimiter has a length
+        // (is not the start of string) and if it matches
+        // field delimiter. If id does not, then we know
+        // that this delimiter is a row delimiter.
+        if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter)) {
+            // Since we have reached a new row of data,
+            // add an empty row to our data array.
+            arrData.push([]);
+        }
+        // Now that we have our delimiter out of the way,
+        // let's check to see which kind of value we
+        // captured (quoted or unquoted).
+        if (arrMatches[2]) {
+            // We found a quoted value. When we capture
+            // this value, unescape any double quotes.
+            var strMatchedValue = arrMatches[2].replace(
+            new RegExp("\"\"", "g"), "\"");
+        } else {
+            // We found a non-quoted value.
+            var strMatchedValue = arrMatches[3];
+        }
+        // Now that we have our value string, let's add
+        // it to the data array.
+        arrData[arrData.length - 1].push(strMatchedValue);
+    }
+    // Return the parsed data.
+    return (arrData);
+}
+
+function CSV2JSON(csv) {
+    var array = CSVToArray(csv);
+    var objArray = [];
+    for (var i = 1; i < array.length; i++) {
+        objArray[i - 1] = {};
+        for (var k = 0; k < array[0].length && k < array[i].length; k++) {
+            var key = array[0][k];
+            objArray[i - 1][key] = array[i][k]
+        }
+    }
+
+    var json = JSON.stringify(objArray);
+    var str = json.replace(/},/g, "},\r\n");
+
+    return str;
+}
