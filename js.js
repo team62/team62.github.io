4b305aa39b74e5ecb53f28669d7d69cd253790ab
Jonathan Damico <jj_damico@yahoo.com>
2016-03-11 09:53:34 -0800

    make it nicer pt 2

--- js.js
+++ js.js
@@ -322,7 +322,10 @@ $(document).ready(function() {
         for(i = 0; i < jd.length; i++) {
           if(jd[i].scored == 'False' && (jd[i].red1 == teamNumber || jd[i].red2 == teamNumber || jd[i].red3 == teamNumber || jd[i].blue1 == teamNumber || jd[i].blue2 == teamNumber || jd[i].blue3 == teamNumber)) {
             differience = jd[i].matchnum - currentMatchNumber;
-            $('#currentmatch').append(', Our Next Match: ' + jd[i].matchnum + ', Up in <b>' + differience + '</b> matches');
+            if(differience == 0)
+              $('#currentmatch').append('<b>Playing Now</b>');
+            else
+              $('#currentmatch').append(', Our Next Match: ' + jd[i].matchnum + ', Up in <b>' + differience + '</b> matches');
             break;
           }
         }
