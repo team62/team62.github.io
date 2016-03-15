a97fa74a157c488acbf3c6ca14699cf0169e30a3
Jonathan Damico <jj_damico@yahoo.com>
2016-03-15 10:07:08 -0700

    accordion

--- js.js
+++ js.js
@@ -364,6 +364,16 @@ $(document).ready(function() {
       async: false,
     });
   }
+  //For Accordion
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function(){
+          this.classList.toggle("active");
+          this.nextElementSibling.classList.toggle("show");
+    }
+  }
 });
 
 function CSVToArray(strData, strDelimiter) {
