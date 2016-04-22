4d2abf4569dbe494272707017fc20387c0b5ec62
Jonathan Damico <jj_damico@yahoo.com>
2016-04-22 09:41:34 -0400

    Update js.js

--- js.js
+++ js.js
@@ -401,7 +401,7 @@ $(document).ready(function() {
             if(differience == 0)
               $('#currentmatch').append('<b> Playing Now</b>');
             else
-              $('#currentmatch').append(', Our Next Match: ' + jd[i].matchnum + ', Up in <b>' + differience + '</b> matches, at ' + jd[i].timescheduled.substring(11);
+              $('#currentmatch').append(', Our Next Match: ' + jd[i].matchnum + ', Up in <b>' + differience + '</b> matches, at ' + (jd[i].timescheduled).substring(11);
             break;
           }
         }
