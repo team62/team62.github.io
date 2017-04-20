66d96b8a9e0b040ddcd59a794d0680144bad8f56
Jonathan Damico <jj_damico@yahoo.com>
2017-04-19 21:16:14 -0400

    boi

--- js.js
+++ js.js
@@ -100,7 +100,7 @@ $(document).ready(function() {
   scoreshtml = '<table style="width:100%" border="1"><tr><th>Match</th><th>Red Alliance</th><th>Blue Alliance</th><th>Red Score</th><th>Blue Score</th><th>Time</th><th>Outcome</th></tr>';
   for (var division = 1; division<=divisions; division++) {
     $.ajax({
-      url: 'https://api.vexdb.io/v1/get_matches?sku=' + mySKU + '&division=' + teamDivision,
+      url: 'https://api.vexdb.io/v1/get_matches?sku=' + mySKU + '&division=' + division,
       dataType: 'json',
       success: function(jd) {
         if(jd!=null) {
